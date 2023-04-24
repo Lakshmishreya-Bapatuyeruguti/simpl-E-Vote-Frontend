2593b8fd6768b13326dd9ac480a468bc73b32f65
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-04-24 22:15:45 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-yellow-300  h-40 w-full flex text-center absolute bottom-0 ">
+    <div className="bg-yellow-300  h-40 w-full flex text-center mt-12 ">
       <h1 className="m-auto text-2xl font-serif underline">About Us</h1>
       <h1 className="m-auto text-2xl font-serif underline">Contact Us</h1>
       <h1 className="m-auto text-2xl font-serif underline">Collaborate</h1>
